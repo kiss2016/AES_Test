3ebeffd403ca7366c55c5c6c6054102e4a003ffa
liumushun <family2us@yahoo.com>
2017-12-03 21:53:50 +0800

    MD5方式加密，输出为大写

---
+++ MD5_Cipher.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import hashlib
+
+#MD5 加密,明文key需要先编码为utf-8
+def md5Encode(key):
+    Md5Cipher = hashlib.md5(key.encode('utf-8')).hexdigest()
+    return Md5Cipher
+
+key = input(u"请输入明文密钥：")
+print(md5Encode(key).upper())
